14bfa27cd4d3d7dc7718ebea4b086ac910928aca
Bjarke Erichsen <97966011+BjarkeErichsen@users.noreply.github.com>
2025-04-30 15:44:32 +0200

    Add files via upload

--- script_task8.py
+++ script_task8.py
@@ -13,7 +13,7 @@ def load_data(load_dir, bid):
 
 
 
-def jacobi(u, interior_mask, max_iter, atol=1e-6):
+def jacobi_original(u, interior_mask, max_iter, atol=1e-6):
     u = np.copy(u)
 
     for i in range(max_iter):
@@ -24,6 +24,7 @@ def jacobi(u, interior_mask, max_iter, atol=1e-6):
         u[1:-1, 1:-1][interior_mask] = u_new_interior
         if delta < atol:
             break
+
     return u
 
 #5.69074 vs 8.7491
@@ -34,9 +35,9 @@ def jacobi(u, u_new, interior_mask):
     #u:  514x514
     if 0 <= x <= interior_mask.shape[0] - 1 and 0 <= y <= interior_mask.shape[1] - 1:
         if interior_mask[x, y]:
-            x += 1
-            y += 1
-            u_new[x, y] = 0.25 * (u[x, y-1] + u[x, y+1] + u[x-1, y] + u[x+1, y])
+            x_u = x + 1
+            y_u = y + 1
+            u_new[x_u, y_u] = 0.25 * (u[x_u, y_u-1] + u[x_u, y_u+1] + u[x_u-1, y_u] + u[x_u+1, y_u])
 
 def summary_stats(u, interior_mask):
     u_interior = u[1:-1, 1:-1][interior_mask]
@@ -50,6 +51,30 @@ def summary_stats(u, interior_mask):
         'pct_above_18': pct_above_18,
         'pct_below_15': pct_below_15,
     }
+
+
+def helper(u0, interior_mask, MAX_ITER):
+    threadsperblock = (16, 16)  # common optimal size for 2D kernels
+
+    u = cuda.to_device(np.copy(u0))
+    u_new = cuda.to_device(np.copy(u0))
+    interior_mask_d = cuda.to_device(interior_mask)
+
+    #u_new is just zeros
+    interior_mask = cuda.to_device(interior_mask)
+    blockspergrid_x = (interior_mask.shape[0] + threadsperblock[0] - 1) // threadsperblock[0]
+    blockspergrid_y = (interior_mask.shape[1] + threadsperblock[1] - 1) // threadsperblock[1]
+    blockspergrid   = (blockspergrid_x, blockspergrid_y)
+    
+    for k in range(MAX_ITER):
+        jacobi[blockspergrid, threadsperblock](u, u_new, interior_mask)
+        u, u_new = u_new, u
+    
+    u = u.copy_to_host()
+
+    return u
+
+
 
 #@profile
 def main():
@@ -70,33 +95,15 @@ def main():
         all_interior_mask[i] = interior_mask
 
     # Run jacobi iterations for each floor plan
-    MAX_ITER = 2000 #20_000
+    MAX_ITER = 20000  #20_000
     ABS_TOL = 1e-4
 
     all_u = np.empty_like(all_u0)
 
-    threadsperblock = (16, 16)  # common optimal size for 2D kernels
 
     for i, (u, interior_mask) in enumerate(zip(all_u0, all_interior_mask)):
         
-        u = cuda.to_device(u)
-        u_new = cuda.device_array_like(u)
-        #u_new is just zeros
-
-
-        interior_mask = cuda.to_device(interior_mask)
-        blockspergrid_x = (interior_mask.shape[0] + threadsperblock[0] - 1) // threadsperblock[0]
-        blockspergrid_y = (interior_mask.shape[1] + threadsperblock[1] - 1) // threadsperblock[1]
-        blockspergrid = (blockspergrid_x, blockspergrid_y)
-        
-        print(blockspergrid_x*threadsperblock[0],  blockspergrid_y * threadsperblock[1])
-        for k in range(MAX_ITER):
-            jacobi[blockspergrid, threadsperblock](u, u_new, interior_mask)
-            #u = cuda.device_array_like(u_new)
-            #u.copy_to_device(u_new)
-            u, u_new = u_new, u
-            
-        u = u.copy_to_host()
+        u = helper(u, interior_mask, MAX_ITER)
         
         all_u[i] = u
 
